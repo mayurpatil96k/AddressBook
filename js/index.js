7a65160bbc4c007a7846e579c4ff9b60d2e3153b
mayurpatil96k <mayur1792001@gmail.com>
2024-04-26 17:37:56 +0530

    [Mayur]Add: Adding solution for usecase 2

--- js/index.js
+++ js/index.js
@@ -3,8 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const contact_1 = require("./contact");
 const createContact_1 = __importDefault(require("./createContact"));
 (function () {
     console.log("Welcome to Address Book Program");
 })();
-(0, createContact_1.default)();
+const AddressBook = new Set();
+AddressBook.add((0, createContact_1.default)());
+for (const contact of AddressBook) {
+    if (contact instanceof contact_1.Contact) {
+        console.log(contact.fName);
+    }
+}

a0e48d34164bccc5ccfc702a288739cf61a7cdb8
mayurpatil96k <mayur1792001@gmail.com>
2024-04-30 11:25:04 +0530

    [Mayur]Add: Adding dependancies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Contact } from "./contact";
 import createContact from "./createContact";
 import PromptSync from "prompt-sync";
+import fs from "fs";
+import { json } from "stream/consumers";
+import { stringify } from "querystring";
 
 const prompt = PromptSync();
 (function () {
@@ -14,6 +17,8 @@ System.set("AddressBook1",AddressBook)
 
 
 AddressBook.add(createContact());
+AddressBook.add(createContact());
+
 
 for (const contact of AddressBook) {
   if (contact instanceof Contact) {
@@ -97,3 +102,109 @@ function getCount(name: string){
 }
 
 // console.log(getCount("Sakri")); get the count of contacts in specific city
+
+function sort() {
+  for (let [key, addressBook] of System) {
+      console.log(`Sorting contacts in AddressBook: ${key}`);
+      let sortedContacts: Contact[] = Array.from(addressBook);
+      sortedContacts.sort((a, b) => a.fName.localeCompare(b.fName as string));
+      System.set(key, new Set(sortedContacts));
+  }
+}
+
+
+function sortByParam(param: string) {
+  for (let [key, addressBook] of System) {
+      console.log(`Sorting contacts in AddressBook: ${key}`);
+      let sortedContacts: Contact[] = Array.from(addressBook);
+
+      sortedContacts.sort((a, b) => {
+          switch (param) {
+              case 'city':
+                  return a.city.localeCompare(b.city as string);
+              case 'state':
+                  return a.state.localeCompare(b.state as string);
+              case 'zip':
+                  return (a.zip as number) - (b.zip as number);
+              default:
+                  throw new Error('Invalid parameter for sorting.');
+          }
+      });
+
+      System.set(key, new Set(sortedContacts));
+  }
+}
+
+// sortByParam("city"); 
+// console.log(System); code usage for sort by parameter use case 12
+
+function writeInFile(){
+  fs.writeFileSync("contacts.txt",JSON.stringify(Array.from(AddressBook)))
+}
+
+
+// writeInFile();
+// make it correct
+
+function readFile(){
+  console.log(fs.readFileSync("contacts.txt",'utf8'))
+}
+// readFile();
+
+
+import { createObjectCsvWriter } from 'csv-writer';
+
+function writeInCsv() {
+
+    const header = [
+        { id: 'fName', title: 'First Name' },
+        { id: 'lName', title: 'Last Name' },
+        { id: 'address', title: 'Address' },
+        { id: 'city', title: 'City' },
+        { id: 'state', title: 'State' },
+        { id: 'zip', title: 'Zip' },
+        { id: 'pNumber', title: 'Phone' },
+        { id: 'email', title: 'Email' }
+    ];
+
+
+    const data = Array.from(AddressBook).map(contact => ({
+      fName: (contact as any).fName,
+      lName: (contact as any).lName,
+      address: (contact as any).address,
+      city: (contact as any).city,
+      state: (contact as any).state,
+      zip: (contact as any).zip,
+      phone: (contact as any).pnumber,
+      email: (contact as any).email
+  }));
+    
+
+    // Create CSV writer
+    const csvWriter = createObjectCsvWriter({
+        path: 'contacts.csv',
+        header: header
+    });
+
+    // Write to CSV file
+    csvWriter.writeRecords(data)
+        .then(() => console.log('CSV file written successfully'))
+        .catch((error) => console.error('Error writing CSV file:', error));
+}
+
+// writeInCsv();
+// Example usage:
+// writeInCsv();
+
+
+function writeInJson(){
+  fs.writeFileSync("json.json",JSON.stringify(Array.from(AddressBook)))
+}
+// writeInJson();
+
+function readOnJson(){
+  const data = fs.readFileSync("json.json", 'utf8');
+  console.log(JSON.parse(data))
+}
+
+// readFile();
